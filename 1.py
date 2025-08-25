b31322249f9ab346623b68732b76023790d4a13d
hitanshi <hitanshiii20@gmail.com>
2025-08-25 21:57:04 +0530

    Add files via upload
    this is 1 python file

---
+++ 1.py
@@ -0,0 +1,19 @@
+#swap two num without using third varaible
+
+# a=int(input("enter a : "))
+# b=int(input("enter b : "))
+
+# print("before swapping ")
+# print("a = ",a," b =",b)
+# a=a+b
+# b=a-b
+# a=a-b
+# print("after swap")
+# print("a =",a,"b = ",b)
+
+a=10
+b=30
+
+a,b=b,a
+print(a)
+print(b)
